9c37714356cd401010cf031de777c06f88f0910a
mrudala <mrudala@gmail.com>
2018-07-18 12:18:07 +0530

    p6 file

---
+++ p6.py
@@ -0,0 +1,11 @@
+# l1=[2,4,5,8]
+# l2=[3,6,7,9]
+# l=zip(l1,l2)
+# a=map(lambda x,y:x+y,l)
+# print(dict(l))
+# l1=[6,7,8,9]
+# l2=[1,2,3,4]
+# l=zip(l1,l2)
+# d=filter(lambda x,y:x+y,l)
+# print(dict(l))
+get pip.py file
